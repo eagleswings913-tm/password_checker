e3db3fa39be9bdff984448a96abb5a57ddba58ce
Tricia Miller <eagleswings913@gmail.com>
2026-06-25 09:51:46 -0400

    Passwrod API

--- check_my_password.py
+++ check_my_password.py
@@ -1,1 +1,13 @@
+# https://haveibeenpwned.com
+# has an API
+# hash the password and send only the first 5 characters of the hash
+# returns all leaked passwords that start with those 5 characters
+# then we check that list for our password
 
+import requests
+
+url = 'https://api.pwnedpasswords.com/range/' + 'cbfda'
+
+res = requests.get(url)
+
+print(res)
